a05d0f1ff5f4c16f1cd1c209f2f4c6b0288b9e63
AndreaHerrera20034 <andreaherrera20034@gmail.com>
2023-01-06 17:27:04 -0600

    Proyecto de la media de numeros

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           Console.WriteLine("=============Bienvenido! :D===============");
+
+           int valores, i, lim, suma = 0;
+           double restas, cuadrados, prome, sumatoria = 0, varianza, desv;
+
+           Console.Write("¿Cuántos números va a ingresar? ");
+           lim = Convert.ToInt32(Console.ReadLine());
+
+           for (i = 0; i <= lim-1; i++)
+           {
+                Console.WriteLine("Ingrese el número: ");
+                valores = Convert.ToInt32(Console.ReadLine());
+                suma = suma + valores;
+           }
+
+           prome = suma / lim;
+
+           Console.WriteLine("La media es: ", prome);
+
+           //restas
+           for(i = 0; i <= lim-1; i++)
+           {
+                restas = valores - prome;
+           }
+
+           //cuadrados y sumatoria
+           for(i = 0; i <= lim-1; i++)
+           {
+                cuadrados = restas * restas;
+                sumatoria = sumatoria + cuadrados;
+           }
+
+           varianza = sumatoria / lim -1;
+           desv =  Math.Sqrt(varianza);
+
+           Console.WriteLine("Varianza es: ", varianza);
+           Console.WriteLine("Desvicación estándar es: ", desv);
+        }
+    }
+}
